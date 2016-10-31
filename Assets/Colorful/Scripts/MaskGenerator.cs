866b83db73f83d2d47a6d1b62ccfd741148857bf
Eclmist <Eclmist@yahoo.com>
2016-10-31 09:21:42 +0800

    Initial Commit

---
+++ Assets/Colorful/Scripts/MaskGenerator.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace UnityStandardAssets.ImageEffects
+{
+    [ExecuteInEditMode]
+    [RequireComponent(typeof(Camera))]
+    [AddComponentMenu("Image Effects/Colorful/MaskGenerator")]
+    public class MaskGenerator : PostEffectsBase
+    {
+        public Shader StencilCullPass;
+        private Material cullMat;
+
+        private RenderTexture tex;
+        private Camera cam;
+
+        // Use this for initialization
+        public override bool CheckResources()
+        {
+            cullMat = CheckShaderAndCreateMaterial(StencilCullPass, cullMat);
+            cam = GetComponent<Camera>();
+            tex = new RenderTexture(cam.pixelWidth, cam.pixelHeight, 24);
+            tex.Create();
+            cam.targetTexture = tex;
+            return base.CheckResources();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        public RenderTexture GetMaskTex()
+        {
+            return tex;
+        }
+
+        public void OnRenderImage(RenderTexture source, RenderTexture destination)
+        {
+            Graphics.Blit(source, destination, cullMat);
+        }
+
+        public void OnDisable()
+        {
+            cam.targetTexture = null;
+        }
+    }
+}
---
+++ Assets/Colorful/Scripts/SelectiveSaturation.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+namespace UnityStandardAssets.ImageEffects
+{
+    [ExecuteInEditMode]
+    [RequireComponent(typeof(Camera))]
+    [AddComponentMenu("Image Effects/Colorful/Selective Saturation")]
+
+    public class SelectiveSaturation : PostEffectsBase
+    {
+
+        public Shader saturationPass;
+        private Material saturationMat;
+
+        [Range(0, 1)] public float saturation;
+        [Range(0, 6)] public float brightness;
+
+        private MaskGenerator maskGen;
+
+        private ManualBlur manualBlur;
+
+        public override bool CheckResources()
+        {
+            CheckSupport(false);
+
+            saturationMat = CheckShaderAndCreateMaterial(saturationPass, saturationMat);
+            maskGen = GetComponentInChildren<MaskGenerator>();
+            manualBlur = GetComponent<ManualBlur>();
+
+            if (!isSupported)
+                ReportAutoDisable();
+            return isSupported;
+        }
+
+
+        public void OnRenderImage(RenderTexture source, RenderTexture destination)
+        {
+            if (CheckResources() == false)
+            {
+                Graphics.Blit(source, destination);
+                return;
+            }
+
+            RenderTexture blurredMaskTex = RenderTexture.GetTemporary(maskGen.GetMaskTex().width, maskGen.GetMaskTex().height);
+
+            manualBlur.Blur(maskGen.GetMaskTex(), blurredMaskTex);
+
+            saturationMat.SetTexture("_MaskTex", blurredMaskTex);
+
+            saturationMat.SetFloat("_Saturation", saturation);
+            saturationMat.SetFloat("_Brightness", brightness);
+
+            Graphics.Blit(source, destination, saturationMat);
+
+            RenderTexture.ReleaseTemporary(blurredMaskTex);
+        }
+    }
+
+}
