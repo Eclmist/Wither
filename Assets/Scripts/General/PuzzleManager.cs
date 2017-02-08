d3bf897bba6ea6d45d0626bd0c800c0d47ce1e9e
ivanleong <ivan_leong_97@hotmail.com>
2017-02-09 00:15:54 +0800

    +Added Level 2 Prefab +Added Obstacles +Added Puzzles +Added PuzzleManager Script +Added Pickups Script (without implementation)

---
+++ Assets/Scripts/General/PuzzleManager.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PuzzleManager : MonoBehaviour {
+
+    protected GameObject[] obstacles;
+    protected Collider colliders;
+
+    void Start()
+    {
+        if(obstacles == null)
+        obstacles = GameObject.FindGameObjectsWithTag("Obstacle");
+        //collider = this.gameObject.GetComponent<Collider>();
+    }
+
+	void Update ()
+    {
+        if (obstacles != null)
+        {
+            PuzzleUpdate();
+        }
+    }
+
+    protected void PuzzleUpdate()
+    {
+        for (int i = 0; i < obstacles.Length; i++)
+        {
+            if (obstacles[i] != null)
+            {
+                colliders = obstacles[i].GetComponent<Collider>();
+
+                if (colliders.enabled == true)
+                {
+                    if (obstacles[i].transform.childCount == 0)
+                    {
+                        Destroy(obstacles[i]);
+                    }
+                }
+            }
+
+        }
+    }
+}
---
+++ Assets/Scripts/Spells/Pickups.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pickups : MonoBehaviour {
+
+    public void PlayerTrigger(Vector3 position, float radius)
+    {
+        Collider[] hitColliders = Physics.OverlapSphere(position, radius, LayerMask.GetMask("PlayerTrigger"));
+
+        for (int i = 0; i < hitColliders.Length; i++)
+        {
+            Destroy(hitColliders[i].gameObject);
+        }
+    }
+
+    public void IvyTrigger(Vector3 position, float radius)
+    {
+        Collider[] hitColliders = Physics.OverlapSphere(position, radius, LayerMask.GetMask("IvyTrigger"));
+
+        for (int i = 0; i < hitColliders.Length; i++)
+        {
+            Destroy(hitColliders[i].gameObject);
+        }
+    }
+}
