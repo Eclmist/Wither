97bde8486443586eb48255561d76148a68d51330
ivanleong <ivan_leong_97@hotmail.com>
2017-02-12 14:19:59 +0800

    +Blocked off level02 +Modified Level03 boss arena +Added Enemy Spawner

---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour {
+
+    //Bound Ranges
+    public float minBoundX;
+    public float maxBoundX;
+    public float minBoundZ;
+    public float maxBoundZ;
+
+    //Spawn Rate per Second
+    [SerializeField]
+    public float spawnRate;
+
+    //Spawn Point
+    protected Vector3 spawnPoint;
+
+    //Enemy Prefab
+    public GameObject enemy;
+
+    //Timer
+    private float timePassed = 0;
+
+    void Update()
+    {
+        if(Time.time - timePassed > spawnRate / (spawnRate * spawnRate))
+        {
+            SpawnEnemy(1);
+            timePassed = Time.time;
+        }
+    }
+
+	public void SpawnEnemy(int enemyCount)
+    {
+        for(int i = 0; i < enemyCount; i++)
+        {
+            spawnPoint = new Vector3(transform.position.x + Random.Range(minBoundX, maxBoundX),
+              transform.position.y,
+              transform.position.z + Random.Range(minBoundZ, maxBoundZ));
+
+            Instantiate(enemy, spawnPoint, enemy.transform.rotation);
+        }
+    }
+}
