f1ae85ad5bfef25930b3992b32723b57fbbdca6c
ivanleong <ivan_leong_97@hotmail.com>
2017-02-12 16:43:12 +0800

    +Added CollapseTree() +Modified level02, level03 (Grass material & Layers)

---
+++ Assets/Scripts/TreeFall.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TreeFall : MonoBehaviour {
+
+    public void CollapseTree()
+    {
+        transform.localEulerAngles = new Vector3(0, -90, 0);
+    }
+
+}
